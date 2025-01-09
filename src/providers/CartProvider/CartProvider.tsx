16c92d7e6b5b5bfc0c479341f99b0ea3b823fb4b
jtabalujan2 <heyyytabalu@yahoo.com>
2025-01-08 21:12:38 -0800

    feat: add provider to handle cart details

---
+++ src/providers/CartProvider/CartProvider.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { createContext, useState, ReactNode } from "react";
+import { produce } from "immer";
+import { Product } from "@/components/MenuItemCardList/MenuItemCardList";
+
+export interface CartStateValue extends Product {
+  quantity: number;
+}
+
+interface CartState {
+  [id: string]: CartStateValue;
+}
+
+interface AddToCartPayload {
+  item: Product;
+  quantity: number;
+}
+
+interface CartContextProps {
+  cart: CartState;
+  addToCart: (payload: AddToCartPayload) => void;
+  removeFromCart?: (id: string) => void;
+  clearCart?: () => void;
+}
+
+export const CartContext = createContext<CartContextProps>({
+  cart: {},
+  addToCart: () => {},
+  removeFromCart: () => {},
+  clearCart: () => {},
+});
+
+interface CartProviderProps {
+  children: ReactNode;
+}
+
+const getExistingCart = () => {
+  const existingCart = localStorage.getItem("cart");
+
+  if (existingCart) {
+    return JSON.parse(existingCart);
+  }
+
+  return {};
+};
+
+export const CartProvider: React.FC<CartProviderProps> = ({ children }) => {
+  const exisitingCart = getExistingCart();
+  const [cart, setCart] = useState<CartState>(exisitingCart);
+
+  const addToCart = ({ item }: AddToCartPayload) => {
+    setCart(
+      produce((drafCart) => {
+        if (drafCart[item?.id]) {
+          drafCart[item.id].quantity += 1;
+        } else {
+          drafCart[item.id] = { ...item, quantity: 1 };
+        }
+
+        localStorage.setItem("cart", JSON.stringify(drafCart));
+      })
+    );
+  };
+
+  const removeFromCart = (id: string) => {
+    setCart(
+      produce((drafCart) => {
+        delete drafCart[id];
+
+        localStorage.setItem("cart", JSON.stringify(drafCart));
+      })
+    );
+  };
+
+  return <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>{children}</CartContext.Provider>;
+};
