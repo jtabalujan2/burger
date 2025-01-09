34aea983ca90d47d6452e8d5ed3a8a792c90b1f3
jtabalujan2 <heyyytabalu@yahoo.com>
2025-01-08 21:27:48 -0800

    fix: routes and hover state

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -11,34 +11,37 @@ import Link from "next/link";
 import { ROUTES } from "@/constants/routes";
 
 export const Cart = () => {
-  const { cart } = useContext(CartContext);
+  const { cart, removeFromCart } = useContext(CartContext);
   const cartItems = Object.entries(cart);
+
+  const handleTrashClick = (id: string) => () => {
+    removeFromCart(id);
+  };
 
   return (
     <>
       <Card className="w-full">
-        <CardTitle>Shopping Cart</CardTitle>
-        <CardContent>
+        <CardTitle className="text-3xl text-center p-6">Shopping Cart</CardTitle>
+        <CardContent className="p-8">
           <ul>
             {cartItems.map(([id, cartItem]) => {
-              console.dir(cartItem);
               const totalPrice = getFormattedPrice({ currency: "USD", price: cartItem.price * cartItem.quantity });
 
               return (
-                <li key={id}>
-                  <Link href={`${ROUTES.MENU}${cartItem.slug}`} className="flex justify-between items-center">
-                    <section className="flex items-center">
-                      <Image src={cartItem.image} alt={cartItem.name} className="rounded-full max-h-12" width={50} height={50} />
-                      <h3 className="p-6">
+                <li key={id} className="flex justify-between items-center">
+                  <Link href={`${ROUTES.MENU}${cartItem.slug}`} className="hover:text-blue-500">
+                    <section className="flex items-center pb-6">
+                      <Image src={cartItem.image} alt={cartItem.name} className="rounded-full max-h-24" width={100} height={90} />
+                      <h3 className="p-6 text-xl">
                         {cartItem.name} ({cartItem.quantity})
                       </h3>
                     </section>
-
-                    <section className="flex items-center">
-                      <p className="pr-6">{totalPrice}</p>
-                      <TrashIcon />
-                    </section>
                   </Link>
+
+                  <section className="flex items-center">
+                    <h3 className="pr-6 text-xl">{totalPrice}</h3>
+                    <TrashIcon className=" hover:text-red-500 transition-colors hover: cursor-pointer" onClick={handleTrashClick(id)} />
+                  </section>
                 </li>
               );
             })}
