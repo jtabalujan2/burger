aecd1237dbf97000cf514faf701edb7fcd4bc3fa
jtabalujan2 <heyyytabalu@yahoo.com>
2025-01-08 23:07:27 -0800

    fix: button styling

--- src/components/CartAddButton/CartAddButton.tsx
+++ src/components/CartAddButton/CartAddButton.tsx
@@ -4,17 +4,23 @@ import { CartContext } from "@/providers/CartProvider/CartProvider";
 import { Button } from "@ui/button";
 import { useContext } from "react";
 import { Product } from "../MenuItemCardList/MenuItemCardList";
+import { cn } from "@/lib/cn";
 
 interface CartAddButtonProps {
   item: Product;
+  className?: string;
 }
 
-export const CartAddButton = ({ item }: CartAddButtonProps) => {
+export const CartAddButton = ({ item, className }: CartAddButtonProps) => {
   const { addToCart } = useContext(CartContext);
 
   const handleClick = () => {
     addToCart({ item, quantity: 1 });
   };
 
-  return <Button onClick={handleClick}>Add to Cart</Button>;
+  return (
+    <Button onClick={handleClick} className={cn(className)}>
+      Add to Cart
+    </Button>
+  );
 };
